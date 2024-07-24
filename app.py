819b3dbcfca36e90e76faa03f00c7beb65352896
Muhammad-Shahin-CS <shahin@ombrulla.com>
2024-07-24 20:52:38 +0530

    Added index and style

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return send_from_directory('.', 'index.html')
+def index():
+    return send_from_directory('.', 'index.html') 
 
 if __name__ == '__main__':
     app.run(debug=True)
