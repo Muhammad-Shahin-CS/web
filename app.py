8f7e0fcd54d790f62e0fce4e664af47cf56d0c5f
Muhammad-Shahin-CS <shahin@ombrulla.com>
2024-07-24 21:49:20 +0530

    Add styles to Flask app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return send_from_directory('.', 'index.html') 
 
 if __name__ == '__main__':
